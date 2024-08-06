1d6b4b6df697255e13dea7d661079a3cdddc4111
Georgios Konstantoulas <gkonstant98@gmail.com>
2024-08-07 02:10:26 +0300

    basic google maps admin dashboard func

--- frontend/lib/widgets/admin_dashboard.dart
+++ frontend/lib/widgets/admin_dashboard.dart
@@ -1,40 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import '../services/auth.dart';
 
 class AdminDashboard extends StatelessWidget {
-  const AdminDashboard({super.key});
+  AdminDashboard({super.key});
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     AuthProvider auth = Provider.of<AuthProvider>(context);
-    double balance = auth.user!.balance;
-    String balanceString = '${balance.toStringAsFixed(2)} EUR';
-
+    
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Dashboard'),
-        automaticallyImplyLeading: false,
-        centerTitle: true,
+        title: const Text("Map"),
+        elevation: 2,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Remaining Balance:',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              balanceString,
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20.0),
-          ],
-        ),
-      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: _center,zoom:11.0),
+      )
     );
   }
 }
