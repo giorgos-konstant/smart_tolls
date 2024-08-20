46b2589598b31f1a020bdd3ce4598c61106bccd6
AurumAsinus <75141045+AurumAsinus@users.noreply.github.com>
2024-08-20 19:51:31 +0300

    Add files via upload
    Backend publish to frontend, backend subscribe to toll topics, backend server modifications.

---
+++ backend/backend_publish.js
@@ -0,0 +1,28 @@
+const mqtt = require('mqtt')
+
+// MQTT Client Setup
+const mqttBroker = 'mqtt://localhost';
+const mqttTopic = 'user/updates';
+const mqttClient = mqtt.connect(mqttBroker);
+
+mqttClient.on('connect', () => {
+    console.log('Connected to MQTT Broker');
+});
+
+const publishUpdate = (mqttData) => {
+    return new Promise((resolve, reject) => {
+        mqttClient.publish(mqttTopic, JSON.stringify(mqttData), {qos: 1}, (err) => {
+            if (err){
+                console.error('Failed to publish MQTT message:', err);
+                reject(err);
+            } else {
+                console.log('MQTT message published successfully');
+                resolve();
+            }
+        });
+    });
+};
+
+module.exports = {
+    publishUpdate
+};
---
+++ backend/backend_subscribe.js
@@ -0,0 +1,35 @@
+const mqtt = require('mqtt')
+const mqttClient = mqtt.connect('mqtt://localhost');
+
+const tolls = ['TEI', 'Konstantinoupoleos', 'Germanou', 'OthonosAmalias'];
+
+// Subscribe to all toll topics
+mqttClient.on('connect', () => {
+    tolls.forEach(toll => {
+        const topic = `tolls/${toll}`;
+        mqttClient.subscribe(topic, (err) => {
+            if (err) {
+                console.error(`Failed to susbscribe to topic ${topic}:`, err);
+            } else {
+                console.log(`Subscribed to topic ${topic}`);
+            }
+        });
+    });
+});
+
+// Handle incoming messages
+mqttClient.on('message', async (topic, message) => {
+    try {
+        const tollName = topic.split('/')[1];
+        const payload = JSON.parse(message.toString());
+        const {device_id, timestamp} = payload;
+
+        // Emit an event when a message is received
+        messageEmitter.emit('tollMessage', {tollName, device_id, timestamp});
+    }
+    catch (error) {
+        console.error('Error processing message:', error);
+    }
+});
+
+module.exports = messageEmitter;
